de1938731def2074eadf4965255a37f6c7a49463
Sasha Gercen <privetpatrick@gmail.com>
2023-04-10 14:38:48 +0300

    routes fixes

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 import "./directory-item.styles.scss";
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(`shop/${title}`);
+
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigateHandler}>
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
