703753a0b82efd050d0ba8d5ed7fa839e4ebc774
Sasha Gercen <privetpatrick@gmail.com>
2023-04-12 10:21:39 +0300

    add redux thunk and loading spinner

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,19 +4,14 @@ import { useDispatch } from "react-redux";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils.js";
-import { setCategories } from "../../store/categories/categories.action";
+import { fetchCategoriesAsync } from "../../store/categories/categories.action";
 import "./shop.styles.scss";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments("categories");
-      dispatch(setCategories(categoriesArray));
-    };
-    getCategoriesMap();
+    fetchCategoriesAsync(dispatch);
   }, []);
 
   return (
