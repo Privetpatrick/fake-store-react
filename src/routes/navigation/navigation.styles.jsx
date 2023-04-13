d5849090b79e9b61e0c8ac53881acc0a351a1ce6
Sasha Gercen <privetpatrick@gmail.com>
2023-04-13 17:42:59 +0300

    add lazy loading

--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -7,12 +7,22 @@ export const NavigationContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and(max-width: 800px) {
+    height: 60px;
+    padding: 10px;
+    margin-bottom: 25px;
+  }
 `;
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+
+  @media screen and(max-width: 800px) {
+    padding: 0;
+    width: 50px;
 `;
 
 export const NavLinks = styled.div`
